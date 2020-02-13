be833fcdbb5556e15d3445ed34d44da4cb641271
rhenriquez <roahenri99@gmail.com>
2020-02-13 02:16:44 -0300

    Commit inicial

---
+++ src/main/java/com/registro/registroapp/RegistroAppApplication.java
@@ -0,0 +1,13 @@
+package com.registro.registroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registro/registroapp/RegistroAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registro.registroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
